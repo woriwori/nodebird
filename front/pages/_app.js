293f7f0007ff94ec145990d7e25db8b8fe8d1ff4
이원희 <wonhee_lee@tmax.co.kr>
2020-02-17 14:07:49 +0900

    initialize

---
+++ front/pages/_app.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+import withRedux from 'next-redux-wrapper';
+import AppLayout from '../components/AppLayout';
+import { applyMiddleware, compose, createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from '../reducers';
+
+const NodeBird = ({ Component, store }) => {
+    return (
+        <Provider store={store}>
+            <Head>
+                <title>NodeBird</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
+            </Head>
+            <AppLayout>
+                <Component />
+            </AppLayout>
+        </Provider>
+    );
+};
+
+NodeBird.propTypes = {
+    Component: PropTypes.elementType,
+    store: PropTypes.object,
+};
+
+export default withRedux((initialState, options) => {
+    const middlewares = [];
+    const enhancer = compose(
+        applyMiddleware(...middlewares),
+        !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f,
+    );
+    const store = createStore(reducer, initialState, enhancer);
+    return store;
+})(NodeBird);
