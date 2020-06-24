1129b327004f0105185db9276cc126626c460aaf
[3기]이원희 <ireba4478@gmail.com>
2020-06-24 23:55:48 +0900

    4-2. redux saga 연동

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -6,6 +6,8 @@ import AppLayout from '../components/AppLayout';
 import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from '../sagas';
 
 const NodeBird = ({ Component, store }) => {
     return (
@@ -27,12 +29,17 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-    const middlewares = []; // redux에 없는 기능을 추가하고 싶을때 미들웨어를 사용
-    const enhancer = compose(
-        applyMiddleware(...middlewares),
-        !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-    ); // redux에서 기본제공되지않는 chrome의 redux devtools를 사용하기 위해 미들웨어를 사용하는 코드임!
+    const sageMiddeware = createSagaMiddleware();
+    const middlewares = [sageMiddeware]; // redux에 없는 기능을 추가하고 싶을때 미들웨어를 사용
+    const enhancer =
+        process.env.NODE_ENV === 'production'
+            ? compose(applyMiddleware(...middlewares))
+            : compose(
+                  applyMiddleware(...middlewares),
+                  !options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+              ); // redux에서 기본제공되지않는 chrome의 redux devtools를 사용하기 위해 미들웨어를 사용하는 코드임!
 
     const store = createStore(reducer, initialState, enhancer);
+    sageMiddeware.run(rootSaga);
     return store;
 })(NodeBird);
